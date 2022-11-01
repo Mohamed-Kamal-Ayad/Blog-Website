5bcf07658818010863e669284100087cb102147f
Mohamed-Kamal-Ayad <mayad@std.mans.edu.eg>
2022-11-02 00:22:23 +0200

    New commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Models\Post;
@@ -7,8 +6,11 @@ use App\Models\Category;
 use App\Models\User;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostCommentsController;
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
+use App\Services\MailchimpNewsletter;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,9 +24,11 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
